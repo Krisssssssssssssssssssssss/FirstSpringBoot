5043457c044faf33a8c8e8920928b2bad326c2db
kristijanjakimov <kiko.jakimov@gmail.com>
2024-09-20 12:28:04 +0200

    POST and DELETE methods added to the controller

---
+++ src/main/java/com/example/FirstSpringBoot/Message.java
@@ -0,0 +1,14 @@
+package com.example.FirstSpringBoot;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private String id;
+    private String name;
+    private String message;
+}
--- src/main/java/com/example/FirstSpringBoot/controller/HelloController.java
+++ src/main/java/com/example/FirstSpringBoot/controller/HelloController.java
@@ -1,14 +1,43 @@
 package com.example.FirstSpringBoot.controller;
+import com.example.FirstSpringBoot.Message;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @RestController
 public class HelloController {
+    private List<Message> messages = new ArrayList<>();
 
     @GetMapping("/api/hello/{name}")
     public String sayHello(@PathVariable String name) {
         return "Hello, " + name + "!";
+    }
+
+
+    @PostMapping("/api/messages")
+    public String createMessage(@RequestBody Message newMessage) {
+        messages.add(newMessage);
+        return "Message received and stored for user: " + newMessage.getName();
+    }
+
+
+    @GetMapping("/api/messages")
+    public List<Message> getAllMessages() {
+        return messages;
+    }
+    @DeleteMapping("/api/messages/{id}")
+    public String deleteMessageById(@PathVariable String id) {
+        Iterator<Message> iterator = messages.iterator();
+        while (iterator.hasNext()) {
+            Message message = iterator.next();
+            if (message.getId().equals(id)) {
+                iterator.remove();  // Remove the message from the list
+                return "Message with ID " + id + " deleted successfully.";
+            }
+        }
+        return "Message with ID " + id + " not found.";
     }
 }
