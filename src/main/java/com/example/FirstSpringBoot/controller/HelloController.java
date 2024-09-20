5a3c37c686efe480c910a4ae08fcf258a18240cf
kristijanjakimov <kiko.jakimov@gmail.com>
2024-09-20 12:14:37 +0200

    Get method with path @pathVariable added

--- src/main/java/com/example/FirstSpringBoot/controller/HelloController.java
+++ src/main/java/com/example/FirstSpringBoot/controller/HelloController.java
@@ -1,13 +1,14 @@
 package com.example.FirstSpringBoot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/api/hello")
-    public String sayHello() {
-        return "Hello, World!";
+    @GetMapping("/api/hello/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hello, " + name + "!";
     }
 }
